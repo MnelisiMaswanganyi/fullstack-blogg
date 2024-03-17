7d38e1e54646047253d09a0e54f2709160d8a523
Mnelisi Maswanganyi <142810215+MnelisiMaswanganyi@users.noreply.github.com>
2024-03-17 15:00:36 +0200

    init client side

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <main>
+      <header>
+        <a href="" className="Logo">The Blog</a> 
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className='post'>
+        <img src='https://assets-global.website-files.com/5e0f1144930a8bc8aace526c/65f46cb7d78a4b3bd08029d9_65f46900f851c380fef1569a-c22a31c353f3dc41aed3dfc82e60072b.jpeg'></img>
+       <div className='texts'>
+        <h2>What is Devin?</h2>
+        <p>Devin is not just a program; it's a groundbreaking AI that acts as a software engineer, capable of coding, debugging, and even developing apps and websites. Created by Cognition and led by Scott Wu, Devin represents a significant leap in AI's role in software development. It's designed to work alongside humans, enhancing productivity rather than replacing jobs. </p>
+        </div>
+      </div>
+      <div className='post'>
+        <img src='https://assets-global.website-files.com/5e0f1144930a8bc8aace526c/65f46cb7d78a4b3bd08029d9_65f46900f851c380fef1569a-c22a31c353f3dc41aed3dfc82e60072b.jpeg'></img>
+       <div className='texts'>
+        <h2>What is Devin?</h2>
+        <p>Devin is not just a program; it's a groundbreaking AI that acts as a software engineer, capable of coding, debugging, and even developing apps and websites. Created by Cognition and led by Scott Wu, Devin represents a significant leap in AI's role in software development. It's designed to work alongside humans, enhancing productivity rather than replacing jobs. </p>
+        </div>
+      </div>
+      <div className='post'>
+        <img src='https://assets-global.website-files.com/5e0f1144930a8bc8aace526c/65f46cb7d78a4b3bd08029d9_65f46900f851c380fef1569a-c22a31c353f3dc41aed3dfc82e60072b.jpeg'></img>
+       <div className='texts'>
+        <h2>What is Devin?</h2>
+        <p>Devin is not just a program; it's a groundbreaking AI that acts as a software engineer, capable of coding, debugging, and even developing apps and websites. Created by Cognition and led by Scott Wu, Devin represents a significant leap in AI's role in software development. It's designed to work alongside humans, enhancing productivity rather than replacing jobs. </p>
+        </div>
+      </div>
+      <div className='post'>
+        <img src='https://assets-global.website-files.com/5e0f1144930a8bc8aace526c/65f46cb7d78a4b3bd08029d9_65f46900f851c380fef1569a-c22a31c353f3dc41aed3dfc82e60072b.jpeg'></img>
+       <div className='texts'>
+        <h2>What is Devin?</h2>
+        <p>Devin is not just a program; it's a groundbreaking AI that acts as a software engineer, capable of coding, debugging, and even developing apps and websites. Created by Cognition and led by Scott Wu, Devin represents a significant leap in AI's role in software development. It's designed to work alongside humans, enhancing productivity rather than replacing jobs. </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
